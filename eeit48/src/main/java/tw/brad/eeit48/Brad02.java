ffda4f7e1c57034a1c2418acaa07a6aa1a14b9fa
BradChao <brad@brad.tw>
2022-08-04 21:58:47 +0800

    init

---
+++ eeit48/src/main/java/tw/brad/eeit48/Brad02.java
@@ -0,0 +1,23 @@
+package tw.brad.eeit48;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Brad02")
+public class Brad02 extends HttpServlet {
+    public Brad02() {
+        super();
+        System.out.println("Brad02()");
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("doGet()");
+	}
+
+
+}
---
+++ eeit48/src/main/java/tw/brad/eeit48/Brad03.java
@@ -0,0 +1,27 @@
+package tw.brad.eeit48;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Brad03
+ */
+@WebServlet("/Brad03")
+public class Brad03 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		Enumeration<String> names = request.getHeaderNames();
+		while( names.hasMoreElements()) {
+			String name = names.nextElement();
+			String value = request.getHeader(name);
+			System.out.println(name +":" +value);
+		}
+	}
+
+}
