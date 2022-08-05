3036d75e4198b8657fbec9e49961654439bffab4
BradChao <brad@brad.tw>
2022-08-05 11:10:30 +0800

    08051110

--- eeit48/src/main/java/tw/brad/eeit48/Brad05.java
+++ eeit48/src/main/java/tw/brad/eeit48/Brad05.java
@@ -1,6 +1,7 @@
 package tw.brad.eeit48;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Enumeration;
 
 import javax.servlet.ServletException;
@@ -17,11 +18,39 @@ public class Brad05 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		
-		Enumeration<String> params = request.getParameterNames();
-		while (params.hasMoreElements()) {
-			String name = params.nextElement();
-			System.out.println(name);
+//		Enumeration<String> params = request.getParameterNames();
+//		while (params.hasMoreElements()) {
+//			String name = params.nextElement();
+//			String value = request.getParameter(name);
+//			System.out.println(name + ":" + value);
+//		}
+		
+		String x = request.getParameter("x");
+		String y = request.getParameter("y");
+		String z = request.getParameter("z");	// null
+		
+		if (z == null) {
+			System.out.println("1");
+		}else if ( z.equals("null")) {
+			System.out.println("2");
+		}else {
+			System.out.println("3");
 		}
+		
+		System.out.println(x +":" + y + ":" + z);
+		
+		int intX = Integer.parseInt(x);
+		int intY = Integer.parseInt(y);
+		int result = intX + intY;
+		
+		//----------------------------
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.printf("%d + %d = %d",intX, intY, result);
+		
+		
+		
+		
 	}
 
 }
