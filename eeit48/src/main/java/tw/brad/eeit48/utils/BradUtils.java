dbeb223eb86c0bf0a73e389e498fd38c6603c652
BradChao <brad@brad.tw>
2022-08-08 15:24:40 +0800

    08081524

--- eeit48/src/main/java/tw/brad/eeit48/utils/BradUtils.java
+++ eeit48/src/main/java/tw/brad/eeit48/utils/BradUtils.java
@@ -1,5 +1,28 @@
 package tw.brad.eeit48.utils;
 
-public class BradUtils {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
+public class BradUtils {
+	public static String loadView() {
+		// 
+		File source = new File("C:\\Users\\USER\\git\\repository5\\eeit48\\src\\main\\webapp\\views\\view1.html");
+		StringBuffer sb = new StringBuffer();
+		try(
+			FileReader fin = new FileReader(source);
+			BufferedReader reader = new BufferedReader(fin)	){
+			
+			String line;
+			while ( (line = reader.readLine()) != null) {
+				sb.append(line);
+			}
+			
+		}catch(Exception e) {
+			System.out.println(e.toString());
+		}
+		
+		
+		return sb.toString();
+	}
 }
