728df0977e3c8fae9c6c3c4d2700e25868004fb0
BradChao <brad@brad.tw>
2022-08-08 13:42:57 +0800

    08081342

---
+++ eeit48/src/main/java/tw/brad/eeit48/Brad09.java
@@ -0,0 +1,30 @@
+package tw.brad.eeit48;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Brad09")
+public class Brad09 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		
+		//-------------------
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>Brad Big Company</h1>");
+		out.print("<hr />");
+		out.print("Hello, World");
+		
+		response.flushBuffer();
+	}
+
+}
