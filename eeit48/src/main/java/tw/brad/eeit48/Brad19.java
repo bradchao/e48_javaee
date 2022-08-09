d9864c0c0070b6ee698b3a06c322a1a08ad5f6fc
BradChao <brad@brad.tw>
2022-08-09 14:30:25 +0800

    08091430

--- eeit48/src/main/java/tw/brad/eeit48/Brad19.java
+++ eeit48/src/main/java/tw/brad/eeit48/Brad19.java
@@ -13,9 +13,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import tw.brad.eeit48.utils.BCrypt;
+
 @WebServlet("/Brad19")
 public class Brad19 extends HttpServlet {
 	private Connection conn;
+	private static final int CHECK_OK = 1;
+	private static final int CHECK_EXCEPTION = 2;
+	private static final int CHECK_ACCOUNT_ERROR = 3;
+	private static final int CHECK_PASSWD_ERROR = 4;
 	
     public Brad19() {
     	try {
@@ -34,32 +40,41 @@ public class Brad19 extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     	String account = req.getParameter("account");
     	String passwd = req.getParameter("passwd");
-    	if (isCheckOK(account, passwd)) {
-    		
-    	}else {
-    		
+    	int status = checkAccount(account, passwd);
+    	switch(status) {
+	    	case CHECK_OK:
+	    		break;
+	    	case CHECK_ACCOUNT_ERROR:
+	    		break;
+	    	case CHECK_PASSWD_ERROR:
+	    		break;
+	    	case CHECK_EXCEPTION:
+	    		break;
     	}
     	super.service(req, resp);
     }
     
-    private boolean isCheckOK(String account, String passwd) {
+    private int checkAccount(String account, String passwd) {
     	String sql = "SELECT * FROM member WHERE account = ?";
     	try {
     		PreparedStatement pstmt = conn.prepareStatement(sql);
     		pstmt.setString(1, account);
     		ResultSet rs = pstmt.executeQuery();
     		if (rs.next()) {
-    			
+    			String passwdEncrypt = rs.getString("passwd");
+    			if (BCrypt.checkpw(passwd, passwdEncrypt)) {
+    				return CHECK_OK;
+    			}else {
+    				return CHECK_PASSWD_ERROR;
+    			}
     		}else {
-    			return false;
+    			return CHECK_ACCOUNT_ERROR;
     		}
     		
     	}catch (Exception e) {
-    		return false;
+    		return CHECK_EXCEPTION;
 		}
     	
-    	
-    	return true;
     }
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
