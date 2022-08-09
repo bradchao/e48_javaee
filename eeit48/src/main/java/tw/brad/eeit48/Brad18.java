62c1e11339f623e5fc8d3be3d235b39e9e62f18a
BradChao <brad@brad.tw>
2022-08-09 11:59:08 +0800

    08091159

---
+++ eeit48/src/main/java/tw/brad/eeit48/Brad18.java
@@ -0,0 +1,48 @@
+package tw.brad.eeit48;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Brad18")
+public class Brad18 extends HttpServlet {
+    public Brad18() {
+    	try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("OK");
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.toString());
+		}
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		showDatabase();
+	}
+
+	private void showDatabase() {
+		Properties prop = new Properties();
+		prop.put("user", "root");
+		prop.put("password", "root");
+		try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/", prop)){
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("show databases");
+			while (rs.next()) {
+				String dbName = rs.getString(1);
+				System.out.println(dbName);
+			}
+			
+			
+		}catch(Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+}
