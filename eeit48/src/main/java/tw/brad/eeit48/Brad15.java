9c11bbfc516c8bf88738267b00cf276d6b803798
BradChao <brad@brad.tw>
2022-08-09 09:27:15 +0800

    08090927

--- eeit48/src/main/java/tw/brad/eeit48/Brad15.java
+++ eeit48/src/main/java/tw/brad/eeit48/Brad15.java
@@ -1,11 +1,17 @@
 package tw.brad.eeit48;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.ImageIcon;
 
 @WebServlet("/Brad15")
 public class Brad15 extends HttpServlet {
@@ -14,6 +20,15 @@ public class Brad15 extends HttpServlet {
 		
 		//------------------
 		response.setContentType("image/jpeg");
+		BufferedImage bimg = new BufferedImage(400, 20, BufferedImage.TYPE_INT_RGB);
+		Graphics2D g2d = bimg.createGraphics();
+		
+		
+		
+		OutputStream out = response.getOutputStream();
+		ImageIO.write(bimg, "jpeg", out);
+		
+		response.flushBuffer();
 	}
 
 }
