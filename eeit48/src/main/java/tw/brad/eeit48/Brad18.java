c9b18219507d3a51ed0950306d3785887d49a261
BradChao <brad@brad.tw>
2022-08-09 14:02:40 +0800

    08091402

--- eeit48/src/main/java/tw/brad/eeit48/Brad18.java
+++ eeit48/src/main/java/tw/brad/eeit48/Brad18.java
@@ -24,23 +24,29 @@ public class Brad18 extends HttpServlet {
 		}
     }
 
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	showDatabase();
+    	super.service(req, resp);
+    }
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		showDatabase();
+    	//showDatabase();
 	}
 
 	private void showDatabase() {
 		Properties prop = new Properties();
 		prop.put("user", "root");
 		prop.put("password", "root");
-		try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/", prop)){
+		try{
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/", prop);
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery("show databases");
 			while (rs.next()) {
 				String dbName = rs.getString(1);
 				System.out.println(dbName);
 			}
-			
-			
+			System.out.println("OK");
 		}catch(Exception e) {
 			System.out.println(e.toString());
 		}
