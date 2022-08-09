2683aee6f046a5261d3849bebda60e1bc1b43d6c
BradChao <brad@brad.tw>
2022-08-09 09:44:40 +0800

    08090944

--- eeit48/src/main/java/tw/brad/eeit48/Brad15.java
+++ eeit48/src/main/java/tw/brad/eeit48/Brad15.java
@@ -1,5 +1,6 @@
 package tw.brad.eeit48;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -18,11 +19,22 @@ public class Brad15 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		
+		String rStr = request.getParameter("rate");
+		int w = 400, h = 20;
+		double rate = 0;	// 50%
+		try {
+			rate = Double.parseDouble(rStr);
+		}catch(Exception e) {}
+		
 		//------------------
 		response.setContentType("image/jpeg");
 		BufferedImage bimg = new BufferedImage(400, 20, BufferedImage.TYPE_INT_RGB);
 		Graphics2D g2d = bimg.createGraphics();
 		
+		g2d.setColor(Color.YELLOW);
+		g2d.fillRect(0, 0, w, h);
+		g2d.setColor(Color.RED);
+		g2d.fillRect(0, 0, (int)rate*w/100, h);
 		
 		
 		OutputStream out = response.getOutputStream();
